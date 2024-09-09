abb9307a05d3ff65fc528e3464c6b63c24318d69
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2024-09-09 14:17:14 -0600

    Ditching fenix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
@@ -16,27 +12,25 @@
     nixpkgs,
     rust-overlay,
     flake-utils,
-    fenix,
-    ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       overlays = [(import rust-overlay)];
       pkgs = import nixpkgs {inherit system overlays;};
-      toolchainToml = builtins.fromTOML (builtins.readFile ./rust-toolchain.toml);
+      rusttoolchain =
+        pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
     in rec {
       # `nix build`
       packages = {
-        # ironhide = (pkgs.makeRustPlatform (fenix.packages.${system}.fromManifest toolchainToml)).buildRustPackage {
         ironhide =
-          (pkgs.makeRustPlatform (fenix.packages.${system}.fromToolchainName {
-            name = toolchainToml.toolchain.channel;
-            sha256 = "sha256-3jVIIf5XPnUU1CRaTyAiO0XHVbJl12MSx3eucTXCjtE=";
-          }))
+          (pkgs.makeRustPlatform {
+            cargo = rusttoolchain;
+            rustc = rusttoolchain;
+          })
           .buildRustPackage {
             # ironhide = pkgs.rustPlatform.buildRustPackage {
             pname = cargoToml.package.name;
-            inherit (cargoToml.package) version name;
+            inherit (cargoToml.package) version;
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
             nativeBuildInputs = with pkgs;
@@ -50,7 +44,7 @@
       # nix develop
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs;
-          [(pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)]
+          [rusttoolchain]
           ++ lib.optionals stdenv.isDarwin
           (with darwin.apple_sdk.frameworks; [Security SystemConfiguration]);
       };
