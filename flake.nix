576ff25123fa13e37b921442cf4f8332f8b3fe37
Patrick Walsh <zmre@users.noreply.github.com>
2024-09-09 09:13:22 -0600

    Fix flake so latest ironhide works on mac (#112)
    * Fix flake so latest ironhide works on mac
    
    * Update time lib and dev shell

--- flake.nix
+++ flake.nix
@@ -7,38 +7,41 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rusttoolchain =
-          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        cargoToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
-
-      in rec {
-        # `nix build`
-        packages = {
-          ironhide = pkgs.rustPlatform.buildRustPackage {
-            pname = cargoToml.package.name;
-            version = cargoToml.package.version;
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-            nativeBuildInputs = with pkgs;
-              [ rusttoolchain ]
-              ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
-                 [ pkgs.darwin.apple_sdk.frameworks.Security ];
-          };
-          default = packages.ironhide;
-        };
-
-        # nix develop
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [ rusttoolchain]
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+      rusttoolchain =
+        pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+    in rec {
+      # `nix build`
+      packages = {
+        ironhide = pkgs.rustPlatform.buildRustPackage {
+          pname = cargoToml.package.name;
+          version = cargoToml.package.version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = with pkgs;
+            [rusttoolchain]
             ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
-               [ pkgs.darwin.apple_sdk.frameworks.Security ];
+            (with pkgs.darwin.apple_sdk.frameworks; [Security SystemConfiguration]);
         };
+        default = packages.ironhide;
+      };
 
-      });
+      # nix develop
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [rusttoolchain]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
+          (with pkgs.darwin.apple_sdk.frameworks; [Security SystemConfiguration]);
+      };
+    });
 }
